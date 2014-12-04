6fb34f8915d82a4faa99ac38dbd5533d3ffa2e7b
zgolman <zgolman@gmail.com>
2014-12-03 21:09:13 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yabe::Application.config.secret_key_base = 'f2d77aa8563c94103e957fb4a0ddd128c30de32c2584f769eca748cd4c839b7b3b642d4923dadcf3163ad1115a9a92c70140fc722f751954c5b7e2035e0ac362'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yabe::Application.config.session_store :cookie_store, key: '_yabe_session'
