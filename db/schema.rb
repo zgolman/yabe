5935f5d2eeac79eefe84295e8d968a3a3a80d51f
Samantha Samuels <your_email@whatever.com>
2014-12-16 15:48:46 -0800

    deleted Auction_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211034231) do
+ActiveRecord::Schema.define(version: 20141216192316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,10 @@ ActiveRecord::Schema.define(version: 20141211034231) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "Auction_id"
+    t.integer  "auction_id"
   end
 
   create_table "products", force: true do |t|
