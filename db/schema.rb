e79e2f6998ce22e22d359b39a4c312e82cc2d1ac
Samantha Samuels <your_email@whatever.com>
2014-12-17 14:32:15 -0800

    added stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217053751) do
+ActiveRecord::Schema.define(version: 20141217195919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20141217053751) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
