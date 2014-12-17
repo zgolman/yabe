c6964425e54ff1613a30e39282e400193926add6
zgolman <zgolman@gmail.com>
2014-12-17 11:40:51 -0800

    rollback if needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217053751) do
+ActiveRecord::Schema.define(version: 20141217192555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,11 +52,9 @@ ActiveRecord::Schema.define(version: 20141217053751) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "auction_id"
-    t.integer  "user_id"
   end
 
   add_index "photos", ["auction_id"], name: "index_photo_id", using: :btree
-  add_index "photos", ["user_id"], name: "index_user_photo_id", using: :btree
 
   create_table "products", force: true do |t|
     t.string   "name"
